b39d3d2bdecd60ca23f989d54449e6f10dee7f4e
andersonalexsandro <anderson.silva.04@outlook.com>
2024-05-30 01:09:18 -0300

    primitive data types

---
+++ modulo-1/primitivateDataTypes.js
@@ -0,0 +1,22 @@
+// String, number, undefined, null, boolean, symbol
+
+const myName = "Anderson" //string
+const num1 = 10 // number
+const num2 = 10.52 //number
+let undefined //underfined
+const none = null
+const bool = true
+
+const a = [1, 2]
+const b = a
+
+console.log(a, b)
+b.push(3)
+console.log(a, b)
+
+console.log(typeof myName)
+console.log(typeof num1)
+console.log(typeof num2)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof bool)
