c4a4e90ebc545604366c328395970e6e9294545a
andersonalexsandro <anderson.silva.04@outlook.com>
2024-05-30 00:14:51 -0300

    variables

---
+++ modulo-1/variaveis/index.js
@@ -0,0 +1,14 @@
+let name = 'Anderson';
+let midlename = 'Leite';
+
+let undefined;
+console.log(undefined);
+
+undefined = 'Agora não está mais undefined';
+
+console.log("Meu nome é " + name);
+console.log(midlename);
+console.log(undefined)
+
+const const_name = "Anderson"
+console.log(const_name)
