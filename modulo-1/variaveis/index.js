60dc58c5c32a02da5d7bf9fcfd9c5fff2d81126f
andersonalexsandro <anderson.silva.04@outlook.com>
2024-05-30 00:46:20 -0300

    more variables

--- modulo-1/variaveis/index.js
+++ modulo-1/variaveis/index.js
@@ -1,14 +1,31 @@
-let name = 'Anderson';
 let midlename = 'Leite';
+let myName = "Anderosn"
 
 let undefined;
 console.log(undefined);
 
 undefined = 'Agora não está mais undefined';
 
-console.log("Meu nome é " + name);
+console.log("Meu nome é " + myName);
 console.log(midlename);
-console.log(undefined)
+console.log(undefined);
 
-const const_name = "Anderson"
-console.log(const_name)
+const const_name = "Anderson";
+console.log(const_name);
+
+console.log( typeof myName);
+let number_test = 10;
+console.log(typeof(number_test));
+
+
+myName = "Anderson";
+const age = "20";
+const high = 1.70
+const weigh = 75
+const imc = weigh / (high ** 2)
+
+console.log(myName + " tem " + age + " anos, pesa " + weigh + " kg \ntem " + high + " de altura e seu IMC é de " + imc.toFixed(2))
+
+let year = 2024
+const born_year = year - parseInt(age)
+console.log(`${myName} nasceu em ${born_year}` )
