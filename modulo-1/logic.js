4115ac31b58b61a15bc55666a21f34264f1c2c5c
andersonalexsandro <anderson.silva.04@outlook.com>
2024-05-31 11:16:23 -0300

    demo page

---
+++ modulo-1/logic.js
@@ -0,0 +1,13 @@
+const dia = true
+
+if(dia){
+    console.log("Bom dia")
+}
+else{
+    console.log("Não é mais bom dia")
+}
+
+for (let i=0; i<10; i++){
+    if(dia) console.log("Bom dia")
+    else console.log("Não é mais dia")
+}
