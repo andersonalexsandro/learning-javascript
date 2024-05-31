1586a3044165dcc6d47bbe201af16af1ac1d12ac
andersonalexsandro <anderson.silva.04@outlook.com>
2024-05-31 00:04:57 -0300

    functions

---
+++ modulo-1/function.js
@@ -0,0 +1,20 @@
+function hello(name){
+    console.log("Hello world " + name)
+}
+hello("Anderson")
+
+
+const power = function (n){
+    return n ** 2
+};
+console.log(power(3))
+
+
+const doubleValue = (n) => {
+    return n * 2
+};
+console.log(doubleValue(2))
+
+
+const doubleValueAgain = n => n * 2
+console.log(doubleValueAgain(3))
