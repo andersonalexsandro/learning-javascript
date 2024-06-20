08d386506563b05aed3549c734b17aafa6fad951
andersonalexsandro <anderson.silva.04@outlook.com>
2024-06-20 19:07:55 -0300

    dealing with prototypes

--- modulo-1/objects/protoypes/prototypes.js
+++ modulo-1/objects/protoypes/prototypes.js
@@ -18,3 +18,32 @@ console.dir(date);
 console.log('####')
 console.log(person1.fullName())
 console.log(person2.fullName())
+
+function Product(name, price){
+    this.name = name;
+    this.price = price;
+}
+
+const p2 = {
+    name: 'FlashLight',
+    price: 50
+};
+
+Object.setPrototypeOf(p2, Product.prototype);
+
+Product.prototype.increase = function (percentual){
+    this.price = this.price + this.price * (percentual/100);
+};
+
+Product.prototype.discount = function (percentual){
+    this.price = this.price - this.price * (percentual/100);
+}
+
+
+let p1 = new Product('Condom', 10);
+p1.increase(100);
+console.log(p1.price);
+
+p2.increase(100);
+console.log(p2.price);
+
