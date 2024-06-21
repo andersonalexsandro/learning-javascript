3a563c91f7fa6495f02a01d99ca856a1794aaccc
andersonalexsandro <anderson.silva.04@outlook.com>
2024-06-21 06:04:14 -0300

    Promises methods

--- modulo-1/assync/promises.js
+++ modulo-1/assync/promises.js
@@ -25,3 +25,25 @@ waitASecond('Phrase 1', rand(1, 3))
         console.log(response);
     })
     .catch();
+
+
+// Example of Promise.resolve
+const promiseResolved = Promise.resolve('Immediately resolved');
+promiseResolved.then(result => console.log(result)); // Output: Immediately resolved
+
+// Example of Promise.reject
+const promiseRejected = Promise.reject('Immediately rejected');
+promiseRejected.catch(error => console.error(error)); // Output: Immediately rejected
+
+// Example of Promise.all
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 'foo');
+});
+
+Promise.all([promise1, promise2, promise3]).then(values => {
+    console.log(values); // Output: [3, 42, "foo"]
+}).catch(error => {
+    console.error(error);
+});
